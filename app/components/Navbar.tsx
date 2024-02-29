95b8b11f76b31775c4198e6a530403eafffed9a7
Salar Shabahang <karishab86@gmail.com>
2024-02-29 15:58:52 +0100

    add route ts / api / create dashboard  URLS conect mit Kind

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,26 @@
 import Link from "next/link";
+import { ThemeToggle } from "./Themetoggle";
+import { Button } from "@/components/ui/button";
+import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 export function Navbar() {
     return (
         <nav className="bg-background h-[10vh] flex items-center">
-            <div className="container flex items-center justify-between">   
-            <Link href="/">
-                <h1>Gudia</h1>
-            </Link>
+            <div className="container flex items-center justify-between">
+                <Link href="/">
+                    <h1 className="font-bold text-3xl">Gudia</h1>
+                </Link>
+                <div className="flex items-center gap-x-5">
+                    <ThemeToggle />
+                    <div className=" flex items-center gap-x-5">
+                        <LoginLink>
+                            <Button>Anmelden</Button>
+                        </LoginLink>
+                        <RegisterLink>
+                            <Button variant="secondary">Konto erstellen</Button>
+                        </RegisterLink>
+                    </div>
+                </div>
             </div>
         </nav>
     )
