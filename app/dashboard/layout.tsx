8827ad47c2e024d42bb76e6c26faed270320cc31
Salar Shabahang <karishab86@gmail.com>
2024-03-12 16:38:50 +0100

    prisma migration / msql user acaunt backend cheng

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,56 @@
 import { ReactNode } from "react";
 import { DashboardNav } from "../components/DashboardNav";
-export default function DashboardLayout({ children }: { children: ReactNode }) {
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+import prisma from "../lib/db";
+import { unstable_noStore as noStore } from "next/cache";
+import { Award } from "lucide-react";
+
+
+async function getData({ email, id, firstName, lastName, profileImage }: {
+    email: string;
+    id: string;
+    firstName: string | undefined | null;
+    lastName: string | undefined | null;
+    profileImage: string | undefined | null;
+}) {
+    noStore();
+    const user = prisma.user.findUnique({
+        where: {
+            id: id,
+        },
+        select: {
+            id: true,
+            stripeCustomerId: true,
+        },
+    });
+
+    if (!user) {
+        const name = `${firstName ?? ""} ${lastName ?? ""}`;
+        await prisma.user.create({
+            data: {
+                id: id,
+                email: email,
+                name: name,
+            },
+        });
+    }
+}
+
+export default async function DashboardLayout({ children }: { children: ReactNode }) {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+    if (!user) {
+        return redirect("/");
+    }
+    await getData({
+        id: user.id as string,
+        email: user.email as string,
+        firstName: user.given_name as string,
+        lastName: user.family_name as string,
+        profileImage: user.picture,
+    });
+
     return (
         <div className="flex flex-col space-y-6 mt-10">
             <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
@@ -10,7 +60,6 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
                 <main>
                     {children}
                 </main>
-
             </div>
         </div>
     );
