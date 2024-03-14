17c2f0b07a668956b76c1c23e175fd94c2e86e11
Salar Shabahang <karishab86@gmail.com>
2024-03-14 10:08:42 +0100

    admin dashboard

--- app/components/DashboardNav.tsx
+++ app/components/DashboardNav.tsx
@@ -9,20 +9,22 @@ export function DashboardNav() {
     const pathname = usePathname(); // usepathname() ist ein hook, das wir in der Datei utils.ts definiert haben
     console.log(pathname);
     return (
-        <nav className="grid items-start gap-2">
-            {navItems.map((item, index) => (
-                <Link key={index} href={item.href}>
-                    <span
-                        className={cn(   // cn ist eine helper function, die wir in der Datei utils.ts definiert haben
-                            "group flex items-center px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-forground",
-                            pathname === item.href ? "bg-accent" : "bg-transparent"
-                        )}
-                    >
-                        <item.icon className="mr-2 h-4 w-4  text-primary" />
-                        <span>{item.name}</span>
-                    </span>
-                </Link>
-            ))}
-        </nav>
+        <div className="bg-background border-cyan-400 border ">
+            <nav className="grid items-start gap-2">
+                {navItems.map((item, index) => (
+                    <Link key={index} href={item.href}>
+                        <span
+                            className={cn(   // cn ist eine helper function, die wir in der Datei utils.ts definiert haben
+                                "group flex items-center px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-forground",
+                                pathname === item.href ? "bg-accent" : "bg-transparent"
+                            )}
+                        >
+                            <item.icon className="mr-2 h-4 w-4  text-primary" />
+                            <span>{item.name}</span>
+                        </span>
+                    </Link>
+                ))}
+            </nav>
+        </div>
     );
 }
