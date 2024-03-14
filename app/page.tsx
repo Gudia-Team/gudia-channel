4ecf25fc5c4ace59731d089b52bfbacaa61d7047
Salar Shabahang <karishab86@gmail.com>
2024-03-14 11:29:48 +0100

    chenge 02

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
       <div className="relative items-center w-full px-5 py-12 mx-auto lg:px-16 max-w-7xl md:px-12">
         <div className="max-w-3xl mx-auto text-center">
           <div>
-            <h1 className="mt-8 text-3xl font-extrabold tracking-tight lg:text-5xl">Willkommen Im Gudia Profesional</h1>
+            <h1 className="text-3xl font-extrabold tracking-tight lg:text-5xl">Willkommen Im Gudia Profesional</h1>
           </div>
           <div className="relative justify-center max-w-sm max-auto mt-2 mx-60 py-5" >
             <RegisterLink>
