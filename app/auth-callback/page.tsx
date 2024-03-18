c286e3976dc20fcd6702470011a27cc52822105c
Salar Shabahang <karishab86@gmail.com>
2024-03-18 14:53:42 +0100

    providers in layout hauptpage add

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { useRouter, useSearchParams } from 'next/navigation';
 import { trpc } from '../_trpc/client';
 import { useEffect } from 'react';
+import { Loader2 } from 'lucide-react';
 
 const Page = () => {
     const router = useRouter();
@@ -22,7 +24,17 @@ const Page = () => {
         }
     }, [origin, query.data?.success, query.isSuccess, router]);
 
-    return null; // oder JSX für Platzhalter- oder Ladezustand
+    return (
+        <div className='w-full mt-24 flex justify-center'>
+            <div className='flex flex-col items-center gap-2'>
+                <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
+                <h3 className='font-semibold text-xl'>
+                    Richten Sie Ihr Konto ein...
+                </h3>
+                <p>Sie werden automatisch weitergeleitet.</p>
+            </div>
+        </div>
+    )
 };
 
 export default Page;
