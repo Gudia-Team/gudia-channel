ee8cc653121f5df35fd63764bdcd2f4f3660ffd4
Salar Shabahang <karishab86@gmail.com>
2024-03-18 15:35:49 +0100

    add trpc

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -1,11 +1,12 @@
+import { appRouter } from '@/app/trpc';
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-import { appRouter } from 'app/trpc';
+
 const handler = (req: Request) =>
   fetchRequestHandler({
     endpoint: '/api/trpc',
     req,
     router: appRouter,
-    createContext: () => ({ ... })
+    createContext: () => ({ })
   });
 
 export { handler as GET, handler as POST };
