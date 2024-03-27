f16767505676185d5e26b1f53a042bcecdad0911
Salar Shabahang <karishab86@gmail.com>
2024-03-27 12:06:47 +0100

    deleted dashboard nav

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -50,7 +50,6 @@ export default async function DashboardLayout({ children, }: { children: ReactNo
             <div className="flex-col space-y-6 mt-10">
                 <div className=" grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
                     <aside className="hidden w-[200px] flex-col md:flex">
-                        <DashboardNav />
                     </aside>
                     <main>
                         {children}
