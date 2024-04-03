219cc0ef6c0667852036b36e2e54df0a137cdbf9
Salar Shabahang <karishab86@gmail.com>
2024-04-03 15:28:30 +0200

    yaya

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,7 +10,7 @@ export default function DashboardPage() {
                 <div className=" ">
                     <Image src={gudia} alt="gudia2" sizes="100vw" quality={100}
                         style={{ objectFit: 'cover', zIndex: '-99', position: 'absolute', top: '0', left: '0', right: '0', bottom: '0'}}
-                        className="border" />
+                        />
                     <div className="flex-row ml-8 gap-7 hidden lg:flex">
                         <NavbarItems label="Home" />
                         <NavbarItems label="Home" />
