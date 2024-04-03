a7a25fabf85f85fa45b1e4313cf7086cc2b7fec5
Salar Shabahang <karishab86@gmail.com>
2024-04-03 13:40:52 +0200

    navitems

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import MaxWidthWrapper from "../components/MaxWidthWrapper";
+import NavbarItems from "../components/NavbarItems";
+import gudia from '../../public/images/gudia.jpg';
 
 export default function DashboardPage() {
     return (
-        <> 
         <MaxWidthWrapper>
-            <h1 className="font-bold text-3xl text-center ">Hallo Hier Ist Dashboard</h1>
+            <div className=" flex gap-4 top-10 left-11 ml-80 items-center ">
+                <NavbarItems label="Home" />
+                <NavbarItems label="Home" />
+                <NavbarItems label="Home" />
+                <NavbarItems label="Home" />
+            </div>
         </MaxWidthWrapper>
-        </>
-
     )
 }
