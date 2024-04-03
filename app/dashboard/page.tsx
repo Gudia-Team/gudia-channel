6440df87ca61fd8db82edb77c7939116d92b67f7
Salar Shabahang <karishab86@gmail.com>
2024-04-03 15:31:07 +0200

    ayay

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,9 +8,9 @@ export default function DashboardPage() {
         <>
             <nav className=" w-full fixed ">
                 <div className=" ">
-                    <Image src={gudia} alt="gudia2" sizes="100vw" quality={100}
-                        style={{ objectFit: 'cover', zIndex: '-99', position: 'absolute', top: '0', left: '0', right: '0', bottom: '0'}}
-                        />
+                    {/* <Image src={gudia} alt="gudia2" sizes="100vw" quality={100}
+                        style={{ objectFit: 'cover', zIndex: '-99', position: 'absolute', top: '0', left: '0', right: '0', bottom: '0' }}
+                    /> */}
                     <div className="flex-row ml-8 gap-7 hidden lg:flex">
                         <NavbarItems label="Home" />
                         <NavbarItems label="Home" />
