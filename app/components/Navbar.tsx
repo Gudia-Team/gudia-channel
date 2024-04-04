e0c16561327d2942015be54016c50137e9d26885
Salar Shabahang <karishab86@gmail.com>
2024-04-04 14:17:07 +0200

    navitem fnish

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,6 +6,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { UserNav } from "./UserNav";
 import React, { useEffect, useState } from 'react';
 import NavbarItems from "./NavbarItems";
+import MaxWidthWrapper from "./MaxWidthWrapper";
 
 
 
@@ -14,32 +15,33 @@ export async function Navbar() {
     const { isAuthenticated, getUser } = getKindeServerSession(); //verbindung zu kinde-auth-nextjs um alle page zu schützen
     const user = await getUser();
     return (
-        <main className="z-50 sticky top-0 inset-x-0 h-16 mt-3 ml-4 mr-4">
-            <div className="flex items-center justify-between">
-                <Link href="/" className="relative justify-start ml-4">
-                    <h1 className="font-bold text-2xl text-primary">Gudia
-                    </h1>
-                    <p className="w-full text-xs loading-none text-muted-foreground">Tv Channel & Journalist </p>
-                </Link>
-                <NavbarItems />
-                <div className="flex items-center gap-x-5 mr-4">
-                    <ThemeToggle />
-                    {(await isAuthenticated()) ? (
-                        <UserNav name={user?.given_name as string} email={user?.email as string} image={user?.picture as string} />
-                    ) : (
-                        <div className=" flex items-center gap-x-5">
-                            <LoginLink>
-                                <Button>Anmelden</Button>
-                            </LoginLink>
-                            <RegisterLink>
-                                <Button variant="secondary">Konto erstellen</Button>
-                            </RegisterLink>
-                        </div>
-                    )}
+        <div className="relative justify-between overflow-hidden">
+            <main className="z-50 sticky top-0 inset-x-0 h-16 mt-3 ml-4 mr-4">
+                <div className="flex items-center justify-between">
+                    <Link href="/" className="relative justify-start ml-4">
+                        <h1 className="font-bold text-2xl text-primary">Gudia
+                        </h1>
+                        <p className="w-full text-xs loading-none text-muted-foreground">Tv Channel & Journalist </p>
+                    </Link>
+                    <NavbarItems />
+                    <div className="flex items-center gap-x-5 mr-4">
+                        <ThemeToggle />
+                        {(await isAuthenticated()) ? (
+                            <UserNav name={user?.given_name as string} email={user?.email as string} image={user?.picture as string} />
+                        ) : (
+                            <div className=" flex items-center gap-x-5">
+                                <LoginLink>
+                                    <Button>Anmelden</Button>
+                                </LoginLink>
+                                <RegisterLink>
+                                    <Button variant="secondary">Konto erstellen</Button>
+                                </RegisterLink>
+                            </div>
+                        )}
+                    </div>
                 </div>
-            </div>
-        </main>
-
+            </main>
+        </div>
     )
 
 }
