be201e77b51793861251244e7ad7d03d018eee9a
Salar Shabahang <karishab86@gmail.com>
2024-04-10 11:24:43 +0200

    movicard title

--- app/components/MovieVideo.tsx
+++ app/components/MovieVideo.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import prisma from '../lib/db';
 
 async function getData() {
@@ -20,7 +21,7 @@ async function getData() {
 export default async function MovieVideo() {
     const data = await getData();
     return (
-        <div className='h-[55vh] lg:h-[60vh] w-full flex justify-center items-center '>
+        <div className='h-[55vh] lg:h-[60vh] w-full flex justify-start items-center '>
             <video
                 poster={data?.thumbnail ?? " "}  // neue änderung für nextjs video achten bitte auf ?? ""
                 autoPlay
@@ -30,6 +31,21 @@ export default async function MovieVideo() {
                 className='w-full absolute top-0 left-0 h-[60vh] object-cover -z-10 brightness-[60%]'
             >
             </video>
+            <div>
+                <div className='relative w-[90%] lg:w-[35%] mx-auto ml-10'>
+                    <h1 className='text-white text-4xl font-bold lg:text-6-xl md:text-5xl '>{data?.title}</h1>
+                    <p className='text-lg mt-5  line-clamp-3'>{data?.description}</p>
+                    <div className='flex gap-x-3 mt-2'>
+                        <Button>
+                            See More
+                        </Button>
+                        <Button>
+                            Watch Now
+                        </Button>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
