6b625107bf656b2beee42ac910d8a700c1ad1dad
Salar Shabahang <karishab86@gmail.com>
2024-04-16 12:10:29 +0200

    kauf chenged

--- app/dashboard/billing/page.tsx
+++ app/dashboard/billing/page.tsx
@@ -47,7 +47,7 @@ async function getData(userId: string) {
 export default async function BillingPage() {
     const { getUser } = getKindeServerSession();
     const user = await getUser();
-    const data = await getData("abs");
+    const data = await getData("");
 
     async function createSubscription() {
         "use server";
@@ -122,7 +122,7 @@ export default async function BillingPage() {
     }
 
     return (
-        <div className="max-w-md mx-auto space-y-4">
+        <div className="container flex flex-col-2 row-auto max-w-md gap-1 sm:max-w-[425px]">
             <Card className="flex flex-col">
                 <CardContent className="py-8">
                     <div>
@@ -132,10 +132,10 @@ export default async function BillingPage() {
                     </div>
 
                     <div className="mt-4 flex items-baseline text-6xl font-extrabold">
-                        $30 <span className="ml-1 text-2xl text-muted-foreground">/mo</span>
+                        $15 <span className="ml-1 text-2xl text-muted-foreground">/mo</span>
                     </div>
                     <p className="mt-5 text-lg text-muted-foreground">
-                        Write as many notes as you want for $30 a Month
+                        Write as many notes as you want for $15 a Month
                     </p>
                 </CardContent>
                 <div className="flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-secondary rounded-lg m-1 space-y-6 sm:p-10 sm:pt-6">
