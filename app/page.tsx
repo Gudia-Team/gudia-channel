2797cc2c4bfcaadc2e196f56bfcb4deb249755aa
Salar Shabahang <karishab86@gmail.com>
2024-04-18 12:57:13 +0200

    add home image

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,8 @@ import { Button } from "@/components/ui/button";
 import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
-import gudia from '../public/images/gudia.jpg';
 import MaxWidthWrapper from "./components/MaxWidthWrapper";
-
+import gudia from "../public/assets/gudia.jpg";
 export default async function Home() {
   const { isAuthenticated } = getKindeServerSession();
   if (await isAuthenticated()) {
@@ -15,13 +14,12 @@ export default async function Home() {
   return (
     <>
       <MaxWidthWrapper >
-        <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl'">
-          <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">Willkommen Im Gudia Profesional</h1>
-          <RegisterLink>
-            <Button size="lg" className="w-60 py-4 ">
-              Get Started Free
-            </Button>
-          </RegisterLink>
+        <div className="py-20 mx-auto text-center flex flex-col items-center">
+          <Image
+            src={gudia}
+            alt="Gudia Logo"
+            className='w-full absolute top-0 left-0 h-[80vh] object-cover -z-10 bg-opacity-90 gry-shadow-lg '
+          ></Image>
         </div>
       </MaxWidthWrapper>
     </>
