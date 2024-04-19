ea5343060a4784096178fca7013e8e07a5027dba
Salar Shabahang <karishab86@gmail.com>
2024-04-19 12:49:00 +0200

    image chenged in movi cards tmdb

--- app/components/MovieCardTmdb.tsx
+++ app/components/MovieCardTmdb.tsx
@@ -1,24 +1,20 @@
+import Image from 'next/image';
 import { baseImgUrl } from '@/lib/constants';
-import { Movie } from '@/models/types'
-
+import { Movie } from '@/models/types';
 
 export default function MovieCardTmdb({ movie }: { movie: Movie }) {
     return (
-        <div className='group bg-zinc-900 clol-span relative h-[12vw]'>
-            <img
+        <div className='group bg-zinc-900 col-span relative h-[12vw]'>
+            <Image
                 src={
                     movie?.backdrop_path || movie?.poster_path
                         ? `${baseImgUrl}${movie?.backdrop_path || movie?.poster_path}`
-                        : "/assets/no-image.png"
+                        : `${baseImgUrl}`
                 }
-                alt='movie?.title || movie?.name'
-                className='cursor-pointer object-cover transition
-                 duration shadow-xl rounded-md
-                  group-hover:opacity-90
-                  sm:group-hover:opacity-0
-                  delay-300
-                  w-full
-                  h-[12vw]'
+                alt={movie?.title || movie?.name}
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                fill
+                className='cursor-pointer object-cover transition duration shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 delay-300'
             />
         </div>
     );
