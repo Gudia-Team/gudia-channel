732c33cb01c9129ccc165d98b7251ca0d2832271
Salar Shabahang <karishab86@gmail.com>
2024-04-23 08:35:14 +0200

    no scroll

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,7 +74,9 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require('tailwindcss-no-scrollbar')],
+  
+
 } satisfies Config
 
 export default config
