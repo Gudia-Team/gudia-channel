b48336dcf721f096fb3d62d58333cc4a5ac82b36
Salar Shabahang <karishab86@gmail.com>
2024-04-23 09:45:17 +0200

    ikjhgkj

--- app/components/CategoryList.tsx
+++ app/components/CategoryList.tsx
@@ -10,19 +10,19 @@ interface Props {
 }
 
 const CategoryList: React.FC<Props> = ({ title, movies }) => {
-    const sliderRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null); // Strongly typed ref object
-    const [scrollPosition, setScrollPosition] = useState<number>(0); // State to track scroll position, typed as number
+    const sliderRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null); 
+    const [scrollPosition, setScrollPosition] = useState<number>(0);
 
-    // Handler for left arrow click
+    // Handler für left arrow click
     const handleLeftClick = () => {
         if (sliderRef.current) {
             const { current: slider } = sliderRef;
-            slider.scrollLeft -= 300; // Adjust this value as needed for your layout
+            slider.scrollLeft -= 300; 
             setScrollPosition(slider.scrollLeft);
         }
     };
 
-    // Handler for right arrow click
+    // Handler für right arrow click
     const handleRightClick = () => {
         if (sliderRef.current) {
             const { current: slider } = sliderRef;
@@ -37,14 +37,14 @@ const CategoryList: React.FC<Props> = ({ title, movies }) => {
             <div className="relative flex items-center w-full h-full sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                 <HiChevronLeft
                     className="md:block text-white text-[40px] text-bold absolute mt-4 cursor-pointer z-10"
-                    onClick={handleLeftClick} // Add onClick event
+                    onClick={handleLeftClick} 
                 />
                 <HiChevronRight
                     className='md:block text-white text-[40px] absolute mt-4 cursor-pointer right-0 z-10'
-                    onClick={handleRightClick} // Add onClick event
+                    onClick={handleRightClick} 
                 />
                 <div className="relative flex w-full h-full overflow-x-auto whitespace-nowrap scroll-smooth scrollbar-none"
-                    ref={sliderRef}> {/* Attach the ref */}
+                    ref={sliderRef}>
                     {movies.map((movie) => (
                         <MovieCardTmdb key={movie.id} movie={movie} />
                     ))}
